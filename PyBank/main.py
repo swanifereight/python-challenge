7dc36202099c159b96035e71d897311875ae5017
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2020-11-18 18:35:45 -0500

    PYbak

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+import os
+import csv
+import datetime
+
+def months_between(start_date, end_date):
+
+    input_file  = os.path.join("python-challenge",'Pybank', 'budget_data.csv')
+if start_date > end_date:
+    raise ValueError(f"Start date {start_date} is not before end date {end_date}")
+    year = start_date.year
+    month = state_date.month
+def input_file(budget_data):{
+
+#total months included in the dataset
+    start_date.year = datetime.date(2010, 1)
+    end_date = datetime.date(2017,2)
+    num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
+    print(num_months)}
+  #  total_months = []
+   # total_profit = []
+   # monthly_profit_change = []
+
+#net total amount of Profit/Losses over the entire period
+#greatest_increase = ["",0]
+#greatest_decrease = ['',0]
