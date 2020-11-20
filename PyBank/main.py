f6ab8bd1c3a0240e2cf2e1d8737b11452bb405d8
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2020-11-20 16:55:43 -0500

    friday

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,7 @@ csv_path = os.path.join('..','Resources', 'budget_data.csv')
 
   
 #total months included in the dataset
-   
+#import the csv file   
 with open(csv_path, "r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
     csv_header = next (csvreader)
@@ -19,7 +19,7 @@ with open(csv_path, "r") as csvfile:
     for row in csvreader:
        #print(row[0])
     
-        total_months.append(row[0])
+        total_months.append(str(row[0]))
         total_profit.append(int(row[1]))
     print(len(total_months))
     print(sum(total_profit))
@@ -28,6 +28,12 @@ with open(csv_path, "r") as csvfile:
     
 
 #net total amount of Profit/Losses over the entire period
+total_months = len(total_months)
+profit_loss = 0
+for x in monthly_profit_change:
+    profit_loss = profit_loss + x
+
 #greatest_increase = ["",0]
 #greatest_decrease = ['',0]
+
 
