d94808c89a6f8d41a91b8bd07082103882141744
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2020-11-20 17:45:00 -0500

    cleaning up

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,31 +1,31 @@
 import os
 import csv
 
-    
-    
+csv_path = os.path.join('..','Resources', 'budget_data.csv')
+
+#Empty lists    
 total_months = []
 total_profit = []
 monthly_profit_change = []
 
-csv_path = os.path.join('..','Resources', 'budget_data.csv')
-
-  
-#total months included in the dataset
-#import the csv file   
+   
 with open(csv_path, "r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
     csv_header = next (csvreader)
     print(f"Header: {csv_header}")
     
     for row in csvreader:
- #net amount of profit and loss     
- #  #print(row[0])
     
         total_months.append(str(row[0]))
         total_profit.append(int(row[1]))
-    print(len(total_months))
-    print(sum(total_profit))
+        print(len(total_months))
+        print(sum(total_profit))
 
+    for x in range(len(total_profit)-1):
+        monthly_profit_change.append(total_profit[x+1]-total_profit[x])
+
+    max_increase_value = max(monthly_profit_change)
+    max_decrease_value = min(monthly_profit_change)
     
 
 
@@ -34,19 +34,6 @@ with open(csv_path, "r") as csvfile:
     
 
 #net total amount of Profit/Losses over the entire period
-total_months = len(total_months)
-profit_loss = 0
-#find revenue change
-for x in range(1, len(total_profit)):
-    revenue_change.append((int(total_profit[x]) - int(total_profit[x-1])))
 
-    revenue_change = []
-    revenue_average = sum(revenue_change)/ len(revenue_change)   
-
-
-#greatest_increase = ["",0]
-greatest_increase = max(revenue_change)
-#greatest_decrease = ['',0]
-greatest_decrease = min(revenue_change)
 
 
